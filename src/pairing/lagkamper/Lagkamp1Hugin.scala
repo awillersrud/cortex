a917db477d21c7400e5e821f2fbb98d5f5e11fda
Asbjørn Willersrud <asbjorn.willersrud@embriq.no>
2016-12-06 09:08:51 +0100

    WTC 2017 Lagkamp 1 & 2

---
+++ src/pairing/lagkamper/Lagkamp1Hugin.scala
@@ -0,0 +1,42 @@
+package pairing.lagkamper
+
+import pairing._
+
+object Lagkamp1Hugin {
+
+  def enemy = Team.create("Munin1", "Cygnar", "Circle", "Legion", "Cryx", "Merc", null)
+
+  def firstPlayerTeam = Team.create("Hugin1",
+    "Jarle",
+    "Asbjørn",
+    "Nico",
+    "Hans",
+    "Kenneth",
+    null)
+
+  /*
+Jarle (Haley2/Haley3): Cygnar: 0, Circle: -1, Legion: 0, Cryx: 1, Mercenaries: 0
+Asbjørn (Wurmwood/Una2): Cygnar: 1, Circle: 0, Legion: 1, Cryx: 1, Mercenaries: 1
+Nicholas (Rahn/Kaelyssa): Cygnar: 0, Circle: 1, Legion: 1, Cryx: -1, Mercenaries: 1
+Hans (Makeda2/Zaadesh2): Cygnar: 0, Circle: -1, Legion 1, Cryx: -1, Mercenaries: 0
+Kenneth (Coven/Skarre1): Cygnar: -1, Circle: -1, Legion: 1, Cryx: 0, Mercenaries: -1
+*/
+
+  val scoreArray = Array(
+    Array(5,4,5,6,5),
+    Array(6,5,6,6,6),
+    Array(5,6,6,4,6),
+    Array(5,4,6,4,5),
+    Array(4,4,6,5,4)
+  )
+
+  def pairing: Pairing = {
+    val pairing = new Pairing(MatchupEvaluations.fromScoreArray(firstPlayerTeam, enemy, scoreArray))
+    pairing
+  }
+
+    def main(args: Array[String]): Unit = {
+      new InteractivePairing(pairing).run()
+    }
+
+}
---
+++ src/pairing/lagkamper/Lagkamp2Hugin.scala
@@ -0,0 +1,44 @@
+package pairing.lagkamper
+
+import pairing._
+
+object Lagkamp2Hugin {
+
+  def enemy = Team.create("Hugin2", "Doomy3/Ragnor", "Denny1/Coven", "Haley2/Haley3", "Amon/HR", "Rahn/Helynna", null)
+
+  def firstPlayerTeam =
+    Team.create("Munin2",
+      "WW/Una",
+      "Issyria/Rahn",
+      "Sev2/HR",
+      "Abby2/Lylyth3",
+      "Denny1/Coven",
+    null)
+
+  /*
+  	Asbjørn	Nico	Christian	Martin	Ole
+Trolls (d3, Ragnor)	60	50	61	55	59
+Cryx (Coven, D1)  	59	49	59	50	50
+Cygnar (H2, H3)	    61	51	50	35	40
+Menoth (HR, Amon)  	50	61	49	40	51
+Ret (Hel, Rahn	    70	60	60	50	51
+   */
+
+  val scoreArray = Array(
+    Array(60,59,61,50,70),
+    Array(50,49,51,61,60),
+    Array(61,59,50,49,60),
+    Array(55,50,35,40,50),
+    Array(59,50,40,51,51)
+  )
+
+  def pairing: Pairing = {
+    val pairing = new Pairing(MatchupEvaluations.fromScoreArray(firstPlayerTeam, enemy, scoreArray))
+    pairing
+  }
+
+    def main(args: Array[String]): Unit = {
+      new InteractivePairing(pairing).run()
+    }
+
+}
